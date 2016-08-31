46b9daf7357d5fedc79e7d20a9f6806b6ce25f5f
Edminsson <axel.aguilar@gmail.com>
2016-08-31 15:38:02 +0200

    added tasks for styles

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # pluralsight-gulp
+This repository is used in the pluralsight course "Ajavascript Build Automation With Guls.js"
+
 Gulp examples with some common packages like jshint and jscs.
 
-It uses gulp-load-plugins to automatically load gulp packages.
-
-It also uses a separate config file for gulp - gulp.config.js that contains some shared objects.
+Some interesting points:
+- It uses gulp-load-plugins to automatically load gulp packages.
+- It also uses a separate config file for gulp - gulp.config.js that contains some shared objects.
+- Originally a callback function was used in a streamless clean-styles task to notify that the task was done.
+I return the del function instead since it uses a promise in the newer versions.
 
 This repository also contains .jscsrs and .jshintrs files that are used by your editor like Code (if you have the right plugins) and the gulp tasks.
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
+var del = require('del');
 
 var $ = require('gulp-load-plugins')({rename: {'gulp-jscs-stylish': 'stylish'}});
 
@@ -22,6 +23,30 @@ gulp.task('vet', function() {
     .pipe($.jshint.reporter('jshint-stylish',{verbose: true}))
     .pipe($.jshint.reporter('fail'));
 });
+
+gulp.task('styles', ['clean-styles'],  function() {
+    log('Compiling less --> CSS to ' + config.temp);
+    return gulp
+    .src(config.less)
+    .pipe($.if(args.verbose, $.print()))
+    .pipe($.less())
+    .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+    .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function() {
+    //This task does not return a stream and needs some other way of telling when it's done.
+    //In the course John uses the done function that was send as a second parameter to the del-function to do this.
+    //It didn't work for me because I'm using a newer version of del that uses promises and I can return the result
+    //from the del function.
+    var files = config.temp + '/**/*.css';
+    log('Cleaning: ' + files);
+    return del(files);
+});
+
+gulp.task('less-watcher', function() {
+    gulp.watch(config.less, ['styles'])
+})
 
 ////////////////
 function log(msg) {
